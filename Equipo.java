152159555bec8fd3073a86fc4435faf69c77b38c
jhon143jm@gmail.com <jsm0092@alu.medac.es>
2026-04-08 14:27:44 +0200

    Ejercicio 4 hecho, se añadieron dos atributos a la clase Equipo

--- Equipo.java
+++ Equipo.java
@@ -1,21 +1,25 @@
+import java.util.ArrayList;
+
 public class Equipo {
 
     private String nombreEquipo;
+    private Entrenador entrenador;
+    private ArrayList<Jugador> jugadores;
 
-//Constructor
+    // Constructor
     public Equipo(String nombreEquipo) {
         this.nombreEquipo = nombreEquipo;
     }
-//Getter
+
+    // Getter
     public String getNombreEquipo() {
         return nombreEquipo;
     }
 
-      //toString
-    
+    // toString
+
     public String toString() {
         return "Equipo [nombreEquipo=" + nombreEquipo + "]";
     }
 
-    
 }
